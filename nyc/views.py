f0beacf687a52eb37afea211dd523abe60165507
Gregg Riess <businessman8994@gmail.com>
2021-11-06 15:30:40 -0400

    Forgot to change title of activity page

--- nyc/views.py
+++ nyc/views.py
@@ -14,5 +14,6 @@ def activity(request, borough, activity):
     if request.method == 'GET':
         return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'venues': boroughs[borough][activity].keys() })
 
-def venue():
-    pass
+def venue(request, borough, activity, venue):
+    if request.method == 'GET':
+        return render(request=request, template_name='venue.html', context={ 'borough': borough, 'activity': activity, 'venues': venue, 'description': boroughs[borough][activity][venue].keys() })
