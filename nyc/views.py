359b46cda30f066f85f1b0368b51d0590c1a6cf3
emmacor <emmacornel83@gmail.com>
2021-11-09 18:25:04 -0500

    Emma

--- nyc/views.py
+++ nyc/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from .boroughs import boroughs
 
+
 def city(request):
     if request.method == 'GET':
-        return render(request=request, template_name='city.html', context={ 'boroughs': boroughs.keys() })
+        return render(request=request, template_name='city.html', context={'boroughs': boroughs.keys()})
+
 
 def borough(request, borough):
     if request.method == 'GET':
-        return render(request=request, template_name='borough.html', context={ 'borough': borough, 'activities': boroughs[borough].keys() })
+        return render(request=request, template_name='borough.html', context={'borough': borough, 'activities': boroughs[borough].keys()})
 
 # Create your views here.
+
+
 def activity(request, borough, activity):
     if request.method == 'GET':
-        return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'venues': boroughs[borough][activity].keys() })
+        return render(request=request, template_name='activity.html', context={'borough': borough, 'activity': activity, 'venues': boroughs[borough][activity].keys()})
+
 
 def venue(request, borough, activity, venue):
     if request.method == 'GET':
-        return render(request=request, template_name='venue.html', context={ 'borough': borough, 'activity': activity, 'venues': venue, 'description': boroughs[borough][activity][venue].keys() })
+        return render(request=request, template_name='venue.html', context={'borough': borough, 'activity': activity, 'venue': venue, 'description': boroughs[borough][activity][venue]['description']})
